b9e39e09b3b8e320f2168fac1c49b684536c6efb
Andre <andre_fabriciow@hotmail.com>
2023-11-08 15:21:23 -0300

    Ideation screen introduced

---
+++ mini3/View/Ideation/IdeationView.swift
@@ -0,0 +1,100 @@
+import SwiftUI
+
+struct IdeationView: View {
+    
+    @State private var currentIndex: Int = 0
+    @GestureState private var dragOffset: CGFloat = 0
+    @State var color = Color.blue
+    @State private var originalWidth: CGFloat = 0
+    @State private var originalHeight: CGFloat = 0
+    
+    
+    var body: some View {
+        GeometryReader{ geometry in
+            let circleSize = geometry.size.width * 0.9
+            
+            ZStack{
+                ForEach(0..<4, id: \.self) { index in
+                    VStack{
+                        CircleView(color: index.isMultiple(of: 2) ? .gray : .cyan, circleSize: .constant(circleSize), index: index)
+                            .scaleEffect(currentIndex == index ? 0.8 : 1.3)
+                            .offset(x: offset(index: index, geometry: geometry, circleSize: circleSize), y: 0)
+                            .disabled(currentIndex != index)
+                    }
+                }
+                
+                HStack {
+                    Button(action: {
+                        withAnimation {
+                            currentIndex = max(0, currentIndex - 1)
+                        }
+                    }) {
+                        Image(systemName: "arrow.left")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .padding()
+                    }
+                    
+                    Spacer()
+                        .frame(width: geometry.size.width - 200, height: geometry.size.height / 2)
+                    
+                    Button(action: {
+                        withAnimation {
+                            currentIndex = min(4 - 1, currentIndex + 1)
+                        }
+                    }) {
+                        Image(systemName: "arrow.right")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .padding()
+                    }
+                }
+                .foregroundColor(.white)
+            }
+            .padding()
+            .frame(width: geometry.size.width, height: geometry.size.height)
+        }
+
+    }
+    
+    private func offset(index: Int, geometry: GeometryProxy, circleSize: CGFloat) -> CGFloat {
+        let overlap = circleSize * 0.05
+        
+        // O offset é então calculado baseando-se no tamanho dos círculos com a sobreposição
+        return CGFloat(index - currentIndex) * (circleSize - overlap)
+    }
+
+
+    
+}
+
+
+#Preview {
+    IdeationView()
+}
+
+struct CircleView: View {
+    var color: Color
+    @Binding var circleSize: CGFloat // Inicializa com um valor padrão
+    @State var index : Int
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .foregroundColor(color)
+                .frame(width: circleSize, height: circleSize)
+
+            VStack{
+                Text("Etapa \(index + 1)") // Mostra o tamanho do círculo
+                Text("Tamanho \(circleSize)")
+            }
+        }
+    }
+}
+
+#Preview {
+    GeometryReader{ geometry in
+        CircleView(color: Color.blue, circleSize: .constant(100), index: 1)
+    }
+}
+
